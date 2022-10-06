2d5281858e6c4f90188f6798300e7c8ff3321899
Daniel Kharms <dkharmsd@gmail.com>
2022-10-07 01:03:58 +0300

    feat(sliceutils): added first methods

---
+++ slice.go
@@ -0,0 +1,40 @@
+package sliceutils
+
+func Unique[T comparable](s []T) []T {
+	var (
+		unique = make([]T, 0, len(s)/2)
+		seen   = make(map[T]struct{})
+	)
+
+	for i := range s {
+		if _, ok := seen[s[i]]; !ok {
+			unique = append(unique, s[i])
+		}
+		seen[s[i]] = struct{}{}
+	}
+
+	return unique
+}
+
+func Filter[T any](s []T, f func(T) bool) []T {
+	passed := make([]T, 0, len(s)/2)
+
+	for i := range s {
+		if f(s[i]) {
+			passed = append(passed, s[i])
+		}
+	}
+
+	return passed
+}
+
+func GroupBy[K comparable, V any](s []V, f func(V) K) map[K][]V {
+	groups := make(map[K][]V)
+
+	for i := range s {
+		group := f(s[i])
+		groups[group] = append(groups[group], s[i])
+	}
+
+	return groups
+}
