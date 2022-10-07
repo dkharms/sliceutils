20875772ec4e0c4fd3736262a0f3e5076e3964f9
Daniel Kharms <dkharmsd@gmail.com>
2022-10-07 19:07:22 +0300

    test(sliceutils): added new tests

--- slice_test.go
+++ slice_test.go
@@ -31,7 +31,7 @@ func TestUnique(t *testing.T) {
 	}
 
 	for _, testCase := range cases {
-		if actual := Unique(testCase.input); !reflect.DeepEqual(actual, testCase.expected) {
+		if actual := Unique(testCase.input...); !reflect.DeepEqual(actual, testCase.expected) {
 			t.Fatalf("expected %v, got %v", testCase.expected, actual)
 		}
 	}
@@ -62,7 +62,7 @@ func TestFilter(t *testing.T) {
 	}
 
 	for _, testCase := range cases {
-		if actual := Filter(testCase.input, testCase.f); !reflect.DeepEqual(actual, testCase.expected) {
+		if actual := Filter(testCase.f, testCase.input...); !reflect.DeepEqual(actual, testCase.expected) {
 			t.Fatalf("expected %v, got %v", testCase.expected, actual)
 		}
 	}
@@ -92,7 +92,7 @@ func TestGroupBy(t *testing.T) {
 	}
 
 	for _, testCase := range cases {
-		if actual := GroupBy(testCase.input, testCase.f); !reflect.DeepEqual(actual, testCase.expected) {
+		if actual := GroupBy(testCase.f, testCase.input...); !reflect.DeepEqual(actual, testCase.expected) {
 			t.Fatalf("expected %v, got %v", testCase.expected, actual)
 		}
 	}
@@ -120,6 +120,104 @@ func TestIntersection(t *testing.T) {
 	for _, testCase := range cases {
 		if actual := Intersect(testCase.x, testCase.y); !reflect.DeepEqual(actual, testCase.expected) {
 			t.Fatalf("expected %v, got %v", testCase.expected, actual)
+		}
+	}
+}
+
+func TestContains(t *testing.T) {
+	type testCase[T comparable] struct {
+		x        []T
+		y        T
+		expected bool
+	}
+
+	cases := []testCase[int]{
+		{
+			x:        []int{4, 5, 6},
+			y:        7,
+			expected: false,
+		},
+		{
+			x:        []int{1, 2, 3},
+			y:        3,
+			expected: true,
+		},
+	}
+
+	for _, testCase := range cases {
+		if actual := Contains(testCase.y, testCase.x...); !reflect.DeepEqual(actual, testCase.expected) {
+			t.Fatalf("expected %v, got %v", testCase.expected, actual)
+		}
+	}
+}
+
+func TestFindIndex(t *testing.T) {
+	type testCase[T comparable] struct {
+		x        []T
+		y        T
+		expected int
+	}
+
+	cases := []testCase[int]{
+		{
+			x:        []int{4, 5, 6},
+			y:        7,
+			expected: -1,
+		},
+		{
+			x:        []int{1, 2, 3},
+			y:        3,
+			expected: 2,
+		},
+	}
+
+	for _, testCase := range cases {
+		if actual := FindIndex(testCase.y, testCase.x...); !reflect.DeepEqual(actual, testCase.expected) {
+			t.Fatalf("expected %v, got %v", testCase.expected, actual)
+		}
+	}
+}
+
+func TestReverse(t *testing.T) {
+	type testCase[T comparable] struct {
+		x        []T
+		expected []T
+	}
+
+	cases := []testCase[int]{
+		{
+			x:        []int{4, 5, 6},
+			expected: []int{6, 5, 4},
+		},
+	}
+
+	for _, testCase := range cases {
+		if actual := Reverse(testCase.x...); !reflect.DeepEqual(actual, testCase.expected) {
+			t.Fatalf("expected %v, got %v", testCase.expected, actual)
+		}
+	}
+}
+
+func TestMap(t *testing.T) {
+	type testCase[T comparable] struct {
+		x        []T
+		f        func(T) T
+		expected []T
+	}
+
+	cases := []testCase[int]{
+		{
+			x: []int{4, 5, 6},
+			f: func(x int) int {
+				return x
+			},
+			expected: []int{4, 5, 6},
+		},
+	}
+
+	for _, testCase := range cases {
+		if Map(testCase.f, testCase.x...); !reflect.DeepEqual(testCase.x, testCase.expected) {
+			t.Fatalf("expected %v, got %v", testCase.expected, testCase.x)
 		}
 	}
 }
